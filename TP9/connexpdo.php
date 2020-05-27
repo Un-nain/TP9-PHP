7febb16c489fe274de9e30f31c43340d065bb193
Un-nain <61002100+Un-nain@users.noreply.github.com>
2020-05-27 20:40:33 +0200

    Add files via upload

---
+++ TP9/connexpdo.php
@@ -0,0 +1,13 @@
+<?php
+
+function connexpdo($base, $user, $password){
+
+    try {
+        $dbh = new PDO($base, $user, $password);
+    } catch (PDOException $e){
+        echo 'Connexion échouée : '.$e->getMessage();
+    }
+    return $dbh;
+}
+
+?>
